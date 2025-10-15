6014ff7dd1880c8c11080a884e0f6cdfa8a19564
KevinClair <kevinclair@apache.org>
2025-10-16 00:00:29 +0800

    生成并打印 JWT 秘钥，用于后续用户认证测试

--- src/test/java/com/github/kevin/oasis/OasisApplicationTests.java
+++ src/test/java/com/github/kevin/oasis/OasisApplicationTests.java
@@ -1,13 +1,21 @@
 package com.github.kevin.oasis;
 
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class OasisApplicationTests {
+import javax.crypto.SecretKey;
+import java.io.UnsupportedEncodingException;
+
+
+public class OasisApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() throws UnsupportedEncodingException {
+        SecretKey secretKey = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+        secretKey.getEncoded();
+        System.out.println("Generated SECRET_KEY: " + java.util.Base64.getEncoder().encodeToString(secretKey.getEncoded()));
     }
 
 }
